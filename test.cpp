6bb8922375f25e98bd6db6dbf7b7db7817b29814
Jacky <lax0xyz@163.com>
2023-06-24 22:00:08 +0800

    添加了部分注释

--- test.cpp
+++ test.cpp
@@ -7,6 +7,12 @@
  * 因为分文件的话，main.cpp中只引入.h，在类模板进行二次编译的时候，无法找到类的定义，会出错
  * 所以要将其声明和定义放到一个头文件中。类模板必须要写在.hpp文件中。*/
 
+/* CMake使用：
+ * .hpp文件无法使用add_library()命令
+ * 直接在上一级目录下的CMakeList.txt使用include_directories()包含就好了
+ * 可以使用file(GLOB ...)命令包含 */
+
+
 #include "MySTL/allocator.hpp"
 #include "MySTL/memory.hpp"
 
